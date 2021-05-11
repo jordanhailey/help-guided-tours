b26f1813e4d6574baf597b88b2b6deacd137c9a9
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-05-11 12:51:40 -0700

    user can make a GET request to /api/museums and be returned JSON data

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import { Controller as MuseumController, Repository as MuseumRepository	} from './museums/index.ts';
 import { createServer } from './web/index.ts'
 
-createServer({configuration:{port: 8080}})
-
 const museumRepository = new MuseumRepository();
+const museumController = new MuseumController({ museumRepository });
 
 // TODO Remove this call to set mock data once data persistence is established
 museumRepository.storage.set("123",{
@@ -13,6 +12,5 @@ museumRepository.storage.set("123",{
 	location: {lat:"12",lng:"34"}
 })
 
-const museumController = new MuseumController({ museumRepository });
-
-console.log(await museumController.getAll());
+createServer({configuration:{port: 8080},museum:museumController})
+//console.log(await museumController.getAll());
