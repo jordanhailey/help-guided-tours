31d09efb1583f9bf387d6229b3429fb6f3d2beee
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-05-11 12:18:58 -0700

    creating class to handle data layer

---
+++ src/museums/repository.ts
@@ -0,0 +1,9 @@
+import type { Museum, MuseumRepository } from './index.ts'
+
+
+export class Repository implements MuseumRepository {
+	storage = new Map<string, Museum>();
+	async getAll() {
+		return [...this.storage.values()];
+	}
+}
