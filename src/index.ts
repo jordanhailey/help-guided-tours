457ad48daad8f3a43d9812276916adac51aae4d3
Jordan Hailey <jordan.m.hailey@gmail.com>
2021-05-11 12:20:43 -0700

    refactoring mock call to museum controller

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-import { Controller as MuseumController	} from './museums/controller.ts';
+import { Controller as MuseumController, Repository as MuseumRepository	} from './museums/index.ts';
 
-const museumController = new MuseumController({
-	museumRepository: {
-		getAll: async () => []
-	}
-});
+const museumRepository = new MuseumRepository();
+
+// TODO Remove this call to set mock data once data persistence is established
+museumRepository.storage.set("123",{
+	id: "123",
+	name: "The Lourve",
+	description: "Museum in Paris",
+	location: {lat:"12",lng:"34"}
+})
+
+const museumController = new MuseumController({ museumRepository });
 
 console.log(await museumController.getAll());
